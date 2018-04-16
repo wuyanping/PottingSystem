5d4cce1863501d04645e028c08ee607714d97b7f
wuyanping <1063218918@qq.com>
2018-04-16 19:02:49 +0800

    [mod] 添加index路由

--- src/assets/js/router/index.js
+++ src/assets/js/router/index.js
@@ -4,12 +4,12 @@ import HelloWorld from 'COMPONENTS/HelloWorld'
 
 // const Index = resolve => require(['VIEWS/index.vue'], resolve)
 
-const Index = resolve => {
-  	// require.ensure 是 Webpack 的特殊语法，用来设置 code-split point
-  	// （代码分块）
-  	require.ensure(['VIEWS/index.vue'], () => {
-  	  	resolve(require('VIEWS/index.vue'))
-  	})
+const index = resolve => {
+    // require.ensure 是 Webpack 的特殊语法，用来设置 code-split point
+    // （代码分块）
+    require.ensure(['VIEWS/index.vue'], () => {
+        resolve(require('VIEWS/index.vue'))
+    })
 }
 
 Vue.use(Router)
@@ -18,8 +18,13 @@ export default new Router({
     routes: [
         {
             path: '/',
-            name: 'HelloWorld',
-            component: HelloWorld
+            name: 'index',
+            component: index
+        },
+        {
+            path: '/index',
+            name: 'index',
+            component: index
         }
     ]
 })
--- src/assets/js/views/index.vue
+++ src/assets/js/views/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-container>
+    <el-container style="height: 100%">
         <el-header>Header</el-header>
         <el-container>
             <el-aside width="200px">Aside</el-aside>
@@ -17,5 +17,38 @@ export default {
 }
 </script>
 
-<style lang="sass">
+<style>
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    /*line-height: 60px;*/
+}
+
+.el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    /*line-height: 200px;*/
+}
+
+.el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    /*line-height: 160px;*/
+}
+
+body > .el-container {
+    margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+    /*line-height: 260px;*/
+}
+
+.el-container:nth-child(7) .el-aside {
+    /*line-height: 320px;*/
+}
 </style>
