4e5bc99924d80eaf800f944587122dae74ea1689
wuyanping <1063218918@qq.com>
2018-04-25 08:54:25 +0800

    [mod] 修改myPotting.vue

--- src/assets/js/wapviews/myPotting/myPotting.vue
+++ src/assets/js/wapviews/myPotting/myPotting.vue
@@ -1,12 +1,9 @@
 <template>
-	<div>
-		<p><span>全部盆栽</span><span>新增盆栽</span></p>
-        <group>
-          <x-input>
-            <img slot="right-full-height" src="https://ws1.sinaimg.cn/large/663d3650gy1fq684go3glj203m01hmwy.jpg">
-          </x-input>
-        </group>
-	</div>
+    <div class="myPotting">
+    	<div class="mp_top">
+    		<div><span>全部盆栽</span><span>新增盆栽</span></div>
+    	</div>
+    </div>
 </template>
 <script>
 import { XInput, Group, XButton, Cell } from 'vux'
