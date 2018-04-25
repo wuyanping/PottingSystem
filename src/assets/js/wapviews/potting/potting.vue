cb61fa3e8a9bab2e56f1b615d76910f4a4af7978
wuyanping <1063218918@qq.com>
2018-04-25 18:30:27 +0800

    [add] 编辑手机端详情页，详情页的详情页

--- src/assets/js/wapviews/potting/potting.vue
+++ src/assets/js/wapviews/potting/potting.vue
@@ -1,23 +1,16 @@
 <template>
 	<div class='potting'>
-		<group>
-            <cell
-                v-for="(pItem,i) in data"
-                :key="i"
-                is-link
-                :link="`/index/potting/${pItem.id}`">
-                <img slot="icon" width="90" style="display:block;margin-right:5px;" :src="pItem.img">
-                <span align-items="flex-start" slot="title">{{pItem.title}}</span>
-                <span slot="inline-desc" class="potting-inlinedesc">{{pItem.inlineDesc}}</span>
-            </cell>
-    	</group>
+		<!-- <list :data="data" :link="link"></list> -->
+        <panel :list="list" :type="type"></panel>
 	</div>
 </template>
 <script>
-import { Cell, CellBox, Group, Badge } from 'vux'
+// import List from '../components/list'
+import { Panel } from 'vux'
 export default {
     components: {
-        Cell, CellBox, Group, Badge
+        // List,
+        Panel
     },
     data () {
         return {
@@ -34,21 +27,26 @@ export default {
                     inlineDesc: 'inlineDesc2',
                     img: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAuCAMAAABgZ9sFAAAAVFBMVEXx8fHMzMzr6+vn5+fv7+/t7e3d3d2+vr7W1tbHx8eysrKdnZ3p6enk5OTR0dG7u7u3t7ejo6PY2Njh4eHf39/T09PExMSvr6+goKCqqqqnp6e4uLgcLY/OAAAAnklEQVRIx+3RSRLDIAxE0QYhAbGZPNu5/z0zrXHiqiz5W72FqhqtVuuXAl3iOV7iPV/iSsAqZa9BS7YOmMXnNNX4TWGxRMn3R6SxRNgy0bzXOW8EBO8SAClsPdB3psqlvG+Lw7ONXg/pTld52BjgSSkA3PV2OOemjIDcZQWgVvONw60q7sIpR38EnHPSMDQ4MjDjLPozhAkGrVbr/z0ANjAF4AcbXmYAAAAASUVORK5CYII='
                 }
+            ],
+            link: 'potting',
+            type: '5',
+            list: [
+                {
+                    // src: 'http://somedomain.somdomain/x.jpg',
+                    src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+                    fallbackSrc: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+                    title: '标题一标题一标题一标题一标题一标题一标题一',
+                    desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。'
+                },
+                {
+                    src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+                    title: '标题二',
+                    desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。'
+                }
             ]
         }
     }
 }
 </script>
 <style lang='sass' scoped>
-.potting {
-    .potting-title{
-        /*display: inline-block;*/
-        /*width: 200px;*/
-    }
-    .potting-inlinedesc{
-        display: inline-block;
-        width: 6rem;
-        word-break:break-all;
-    }
-}
 </style>
