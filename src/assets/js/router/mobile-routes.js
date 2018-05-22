56448acebbf6084022bd469d48e3c9985eadc7fd
wuyanping <1063218918@qq.com>
2018-05-22 17:00:39 +0800

    [mod] 更新 路由

--- src/assets/js/router/mobile-routes.js
+++ src/assets/js/router/mobile-routes.js
@@ -1,24 +1,31 @@
 // =============================顶层组件====================================
 // ---------------------------最顶层组件----------------------------------
-const index = resolve => require(['WAPVIEWS/index.vue'], resolve)
+// const index = resolve => require(['WAPVIEWS/index.vue'], resolve)
+import index from 'WAPVIEWS/index.vue'
 
 // ---------------------------登录组件----------------------------------
-const login = resolve => require(['WAPVIEWS/login.vue'], resolve)
+// const login = resolve => require(['WAPVIEWS/login.vue'], resolve)
+import login from 'WAPVIEWS/login.vue'
 
 // ---------------------------404----------------------------------
-const notFound = resolve => require(['VIEWS/404.vue'], resolve)
+// const notFound = resolve => require(['VIEWS/404.vue'], resolve)
+import notFound from 'VIEWS/404.vue'
 
 // ---------------------------首页----------------------------------
-const model = resolve => require(['WAPVIEWS/components/model/model.vue'], resolve)
+// const model = resolve => require(['WAPVIEWS/components/model/model.vue'], resolve)
+import model from 'WAPVIEWS/components/model/model.vue'
 
 // --------------------------- 一级详情页 ----------------------------------
-const details = resolve => require(['WAPVIEWS/components/details/details.vue'], resolve)
+// const details = resolve => require(['WAPVIEWS/components/details/details.vue'], resolve)
+import details from 'WAPVIEWS/components/details/details.vue'
 
 // --------------------------- 二级详情页 ----------------------------------
-const recordDetails = resolve => require(['WAPVIEWS/components/recordDetails/recordDetails.vue'], resolve)
+// const recordDetails = resolve => require(['WAPVIEWS/components/recordDetails/recordDetails.vue'], resolve)
+import recordDetails from 'WAPVIEWS/components/recordDetails/recordDetails.vue'
 
 // --------------------------- 盆栽管理 ----------------------------------
-const potManage = resolve => require(['WAPVIEWS/userInfo/Details/potManage.vue'], resolve)
+// const potManage = resolve => require(['WAPVIEWS/userInfo/Details/potManage.vue'], resolve)
+import potManage from 'WAPVIEWS/userInfo/Details/potManage.vue'
 
 let routes = [
     {
--- src/assets/js/router/pc-routes.js
+++ src/assets/js/router/pc-routes.js
@@ -1,17 +1,22 @@
 // =============================顶层组件====================================
 // ---------------------------最顶层组件----------------------------------
-const index = resolve => require(['VIEWS/index.vue'], resolve)
+// const index = resolve => require(['VIEWS/index.vue'], resolve)
+import index from 'VIEWS/index.vue'
 
 // ---------------------------登录组件----------------------------------
-const login = resolve => require(['VIEWS/login.vue'], resolve)
+// const login = resolve => require(['VIEWS/login.vue'], resolve)
+import login from 'VIEWS/login.vue'
 
 // ---------------------------404----------------------------------
-const notFound = resolve => require(['VIEWS/404.vue'], resolve)
+// const notFound = resolve => require(['VIEWS/404.vue'], resolve)
+import notFound from 'VIEWS/404.vue'
 
 // ---------------------------首页----------------------------------
-// const home = resolve => require(['PAGE/home/home.vue'], resolve)
-const userInfo = resolve => require(['PAGE/top/components/userInfo.vue'], resolve)
-const model = resolve => require(['PAGE/model/model.vue'], resolve)
+// const userInfo = resolve => require(['PAGE/top/components/userInfo.vue'], resolve)
+// const model = resolve => require(['PAGE/model/model.vue'], resolve)
+
+import userInfo from 'PAGE/top/components/userInfo.vue'
+import model from 'PAGE/model/model.vue'
 
 let routes = [
     {
