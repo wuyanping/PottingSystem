fe80288053a1506b9f036b64878e1c431441038d
wuyanping <1063218918@qq.com>
2018-06-04 09:08:38 +0800

    [mod] wapviews/components/model/model.vue

--- src/assets/js/wapviews/components/model/model.vue
+++ src/assets/js/wapviews/components/model/model.vue
@@ -27,33 +27,6 @@ export default {
     },
     data () {
         return {
-            // list: [
-            //     {
-            //         id: 0,
-            //         src: 'http://somedomain.somdomain/x.jpg',
-            //         fallbackSrc: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
-            //         title: '标题一标题一标题一标题一标题一标题一标题一',
-            //         desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。'
-            //         // ,
-            //         // url: '/component/cell'
-            //     },
-            //     {
-            //         id: 2,
-            //         src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
-            //         title: '标题二',
-            //         desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。'
-            //         // ,
-            //         // url: {
-            //         //     path: '/component/radio',
-            //         //     replace: false
-            //         // },
-            //         // meta: {
-            //         //     source: '来源信息',
-            //         //     date: '时间',
-            //         //     other: '其他信息'
-            //         // }
-            //     }
-            // ],
             currentModel: {},
             routePath: '',
             hasCustomComponent: true
